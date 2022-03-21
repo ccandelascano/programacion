ee655e9740506654c688894c542470b8e5cce221
ccandelascano <101114841+ccandelascano@users.noreply.github.com>
2022-03-21 10:13:54 -0300

    Add files via upload

---
+++ cliente (1).sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 21-03-2022 a las 14:13:19
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `empresa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `DNI` int(11) NOT NULL,
+  `teléfono` int(11) DEFAULT NULL,
+  `domicilio` varchar(50) NOT NULL,
+  `edad` int(11) DEFAULT NULL,
+  `fecha_nac` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nombre`, `apellido`, `DNI`, `teléfono`, `domicilio`, `edad`, `fecha_nac`) VALUES
+(1, 'Camila', 'Couretot', 46127903, 2147483647, 'Yrigoyen', 17, '2004-12-18'),
+(2, 'Candelaria', 'Lascano', 45934644, 2147483647, 'Belisle 2805', 17, '2004-10-04'),
+(3, 'Angelica', 'Dutto', 26100889, 155204829, 'Yrigoyen', 42, '1978-11-21'),
+(4, 'Patricia', 'Carranza', 25322073, 155336187, 'Belisle 2805', 45, '1976-08-15'),
+(5, 'Roberto', 'Couretot', 27078459, 155642690, 'Yrigoyen', 43, '1979-01-08'),
+(8, 'Candelaria', 'Lascano', 45934644, 1553036490, 'Belisle 2805', 17, '2004-10-04'),
+(9, 'Patricia ', 'Carranza', 25322073, 155326187, 'Belisle 2805', 45, '1976-08-15'),
+(10, 'Camila', 'Couretot', 46127903, 152045093, 'Yrigoyen 354', 17, '2004-12-18'),
+(11, 'Angélica', 'Dutto', 26100889, 15642690, 'Yrigoyen 354', 43, '1978-11-21'),
+(14, 'Roberto', 'Couretot', 27078459, 15204829, 'Yrigoyen 354', 43, '1979-01-08');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
